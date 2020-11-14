25064fecbd27a85c582490c2cb2dc6b876ea8619
JUAN CARLOS <juanchoyvanessa@hotmail.com>
2020-11-13 20:08:50 -0500

    practica final calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+function suma (){
+    var numero1 = parseFloat(document.getElementById('numero1').Value);
+    var numero2 = parseFloat(document.getElementById('numero2').Value);
+    var Resultado = numero1 + numero2;
+
+    document.getElementById('Resultado').value = Resultado;
+}
