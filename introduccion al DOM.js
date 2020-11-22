a17eafbb7297f5bbe0798bf52ba540e3ba8d5656
JUAN CARLOS <juanchoyvanessa@hotmail.com>
2020-11-22 09:24:15 -0500

    realizando cambios en el DOm con javascript aprendiendo a  quitar y poner nombres en los objetos aprendiendo a cambiar o quitar un objeto dentro del DOm de javascript

--- introduccion al DOM.js
+++ introduccion al DOM.js
@@ -1,13 +1,41 @@
 //Accediendo al DOM: example:
 
-//var cajas = document.getElementsByTagName('div');
-//cajas[0].textcontent = 'primera caja';
-//cajas[0].innerHTML = <h1>primera</h1>;
+var cajas = document.getElementsByTagName('div');
+//var cajas = document.getElementsByClassName('caja');
 
-var cajas = document.getElementsByClassName('caja');
-//cajas[0].textcontent = 'primera caja';
+var primeracaja = document.getElementById('primeracaja');
 
-var primeraCaja = document.getElementById('primeraCaja')
-primeraCaja.textcontent = 'primera Caja';
+primeracaja.textContent = 'Hola Mundo';
+
+//cajas[0].textContent = 'Hola Mundo';
+//cajas[0].innerHTML = '<h1>Hola</h1>';
+
+//creando Nodos cinco pasos
+var caja = document.createElement('div');  // 1-creo el nodo de la caja con div
+var contenido = document.createTextNode('Hola Mundo 2'); //2- creando el nodo contenido que es texto
+caja.appendChild(contenido);// 3-uniendo nodo de caja con nodo texto o contenido
+//caja.setAttribute('class','caja naranja');// 4- dandole atributos a mi caja o nodo
+
+/*var contenedor = document.getElementById('contenedor'); //accedo o llamo  al nodo section con el id
+contenedor.appendChild(caja); // 5- ahora uniendo mi nodo section-contenedor con la caja que yo cree*/
+
+//4- otra forma de cambiar el id o la clase de mi caja 
+caja.id = 'primera';
+caja.className = 'caja naranja';
+
+//como saber el contenedor padre?
+var padre = cajas[0].parentNode; // se observa dentro de la consola
+
+//
+// 5- otra forma de unir mi caja en la posicion que yo quiero 
+// (ahora uniendo mi nodo section-contenedor con la caja que yo cree)
+
+ /*padre.insertBefore(caja , primeracaja);*/
+
+ //remplazar una caja o elemento hijo
+ padre.replaceChild(caja , cajas[2]);
+
+ //eliminar una caja o elemento hijo
+ padre.removeChild(cajas[3]);
 
 
